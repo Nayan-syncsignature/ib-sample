5b0dde96d97378bcdca8a8d360befe22e47ffdab
Nayan-syncsignature <nayan.syncsignature@gmail.com>
2025-04-23 14:35:50 +0530

    templat-1 style changes

--- components/instant-branding/banner/template-1/styles.ts
+++ components/instant-branding/banner/template-1/styles.ts
@@ -30,9 +30,14 @@ export const styles: StyleItem[] = [
       },
       textColor: "white",
       backgroundBackdropConfig: {
-        backgroundBackdropUrl: "https://i.postimg.cc/WbJNvqnF/img.png",
-        backgroundBackdropPosition: "right center",
-        backgroundBackdropSize: "contain",
+        backgroundBackdropUrl: "",
+        backgroundBackdropPosition: "",
+        backgroundBackdropSize: "",
+      },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "https://i.postimg.cc/WbJNvqnF/img.png",
+        headshotBackdropPosition: "right center",
+        headshotBackdropSize: "contain",
       },
       backgroundStyle: {
         type: "solid",
@@ -63,9 +68,14 @@ export const styles: StyleItem[] = [
       },
       textColor: "white",
       backgroundBackdropConfig: {
-        backgroundBackdropUrl: "https://i.postimg.cc/fb3CkLwy/Ellipse-34.png",
-        backgroundBackdropPosition: "right bottom",
+        backgroundBackdropUrl: "",
+        backgroundBackdropPosition: "",
         backgroundBackdropSize: "",
+      },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "https://i.postimg.cc/fb3CkLwy/Ellipse-34.png",
+        headshotBackdropPosition: "right bottom",
+        headshotBackdropSize: "",
       },
       backgroundStyle: {
         type: "solid",
@@ -96,9 +106,14 @@ export const styles: StyleItem[] = [
       },
       textColor: "white",
       backgroundBackdropConfig: {
-        backgroundBackdropUrl: "https://i.postimg.cc/7Zxwwdrt/Rectangle-48.png",
-        backgroundBackdropPosition: "right bottom",
+        backgroundBackdropUrl: "",
+        backgroundBackdropPosition: "",
         backgroundBackdropSize: "",
+      },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "https://i.postimg.cc/7Zxwwdrt/Rectangle-48.png",
+        headshotBackdropPosition: "right bottom",
+        headshotBackdropSize: "",
       },
       backgroundStyle: {
         type: "solid",
@@ -134,9 +149,9 @@ export const styles: StyleItem[] = [
         backgroundBackdropSize: "",
       },
       headshotBackdropConfig: {
-        headshotBackdropUrl: "https://i.postimg.cc/2yK1JrBL/Vector-1.png",
-        headshotBackdropPosition: "left bottom",
-        headshotBackdropSize: "contain",
+        headshotBackdropUrl: "https://i.postimg.cc/6qQcd9N7/Rectangle-80.png",
+        headshotBackdropPosition: "right bottom",
+        headshotBackdropSize: "",
       },
       backgroundStyle: {
         type: "solid",
@@ -146,6 +161,44 @@ export const styles: StyleItem[] = [
   },
   {
     template_key: 5,
+    data: {
+      heading: "Build a Memorable Personal Brand, Without Hiring a Team",
+      description:
+        "Your all-in-one personal brand OS — from strategy to content, made simple.",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/707e/c151/75eeca732fe81415fc6105b91f38697f?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=SrN5DdIFFqvCpiAvAqXW85YS0W6yCIPBySsFyUAZFftWH~BRfVTfq6Wn3HE5sHPc6I6P4Ctrq4-M87sKBKVaqZvl2xTirExbOtormQ4QMNcVxwXyHWr2eH7JDeCe3Rz0BPZkrXbxQqkIUy86mJJ5h7YYr-zeTGE6ao7BtcoC8P1qGjg4XbXR8YsifZIMn4pnYpaYoSi1wNOBolik4z~VMc6My2OKYqlJSq28KhqL4NVTXs79Wc5Shu6LCQ09BzU02U3iGP8nMvvOisqhsjeUnkwbR~8vDliij6CkAB1TKLLDFRWNBvrBASMbgH38cYtMRu-7aHyQJLgW1d20yHpr4Q__", // Replace with your actual image
+      imageAlt: "Professional headshot",
+      buttonText: "Get Started",
+      communityButtonText: "JOIN OUR COMMUNITY",
+      ctaButtonText: "Start Free Trial",
+    },
+    commonConfig: {
+      width: 1584, // Use width from useCanvasDimensions hook
+      height: 396, // Use height from useCanvasDimensions hook
+      fontSize: {
+        heading: "48px",
+        description: "32px",
+        button: 20,
+      },
+      textColor: "white",
+      backgroundBackdropConfig: {
+        backgroundBackdropUrl: "https://i.postimg.cc/2yK1JrBL/Vector-1.png",
+        backgroundBackdropPosition: "left bottom",
+        backgroundBackdropSize: "contain",
+      },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "https://i.postimg.cc/WbJNvqnF/img.png",
+        headshotBackdropPosition: "right center",
+        headshotBackdropSize: "contain",
+      },
+      backgroundStyle: {
+        type: "solid",
+      },
+      contentBackgroundColor: "false",
+    },
+  },
+  {
+    template_key: 6,
     data: {
       heading: "Build a Memorable Personal Brand, Without Hiring a Team",
       description:
@@ -185,7 +238,7 @@ export const styles: StyleItem[] = [
     },
   },
   {
-    template_key: 6,
+    template_key: 7,
     data: {
       heading: "Build a Memorable Personal Brand, Without Hiring a Team",
       description:
@@ -224,7 +277,7 @@ export const styles: StyleItem[] = [
     },
   },
   {
-    template_key: 7,
+    template_key: 8,
     data: {
       heading: "Build a Memorable Personal Brand, Without Hiring a Team",
       description:
@@ -268,46 +321,6 @@ export const styles: StyleItem[] = [
     },
   },
   {
-    template_key: 8,
-    data: {
-      heading: "Build a Memorable Personal Brand, Without Hiring a Team",
-      description:
-        "Your all-in-one personal brand OS — from strategy to content, made simple.",
-      imageUrl:
-        "https://s3-alpha-sig.figma.com/img/707e/c151/75eeca732fe81415fc6105b91f38697f?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=SrN5DdIFFqvCpiAvAqXW85YS0W6yCIPBySsFyUAZFftWH~BRfVTfq6Wn3HE5sHPc6I6P4Ctrq4-M87sKBKVaqZvl2xTirExbOtormQ4QMNcVxwXyHWr2eH7JDeCe3Rz0BPZkrXbxQqkIUy86mJJ5h7YYr-zeTGE6ao7BtcoC8P1qGjg4XbXR8YsifZIMn4pnYpaYoSi1wNOBolik4z~VMc6My2OKYqlJSq28KhqL4NVTXs79Wc5Shu6LCQ09BzU02U3iGP8nMvvOisqhsjeUnkwbR~8vDliij6CkAB1TKLLDFRWNBvrBASMbgH38cYtMRu-7aHyQJLgW1d20yHpr4Q__", // Replace with your actual image
-      imageAlt: "Professional headshot",
-      buttonText: "Get Started",
-      communityButtonText: "JOIN OUR COMMUNITY",
-      ctaButtonText: "Start Free Trial",
-    },
-    commonConfig: {
-      width: 1584, // Use width from useCanvasDimensions hook
-      height: 396, // Use height from useCanvasDimensions hook
-      fontSize: {
-        heading: "48px",
-        description: "32px",
-        button: 20,
-      },
-      textColor: "white",
-      backgroundBackdropConfig: {
-        backgroundBackdropUrl: "",
-        backgroundBackdropPosition: "",
-        backgroundBackdropSize: "",
-      },
-      backgroundStyle: {
-        type: "linear",
-        config: {
-          direction: "to left",
-          stops: [
-            { position: "0%", opacity: "100%" },
-            { position: "70%", opacity: "100%" },
-          ],
-        },
-      },
-      contentBackgroundColor: "false",
-    },
-  },
-  {
     template_key: 9,
     data: {
       heading: "Build a Memorable Personal Brand, Without Hiring a Team",
@@ -334,10 +347,19 @@ export const styles: StyleItem[] = [
         backgroundBackdropPosition: "",
         backgroundBackdropSize: "",
       },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "",
+        headshotBackdropPosition: "",
+        headshotBackdropSize: "",
+      },
       backgroundStyle: {
-        type: "radial",
+        type: "linear",
         config: {
-          circles: [{ position: "bottom left", size: "600px", opacity: "2%" }],
+          direction: "to left",
+          stops: [
+            { position: "0%", opacity: "100%" },
+            { position: "70%", opacity: "100%" },
+          ],
         },
       },
       contentBackgroundColor: "false",
@@ -370,6 +392,52 @@ export const styles: StyleItem[] = [
         backgroundBackdropPosition: "",
         backgroundBackdropSize: "",
       },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "",
+        headshotBackdropPosition: "",
+        headshotBackdropSize: "",
+      },
+      backgroundStyle: {
+        type: "radial",
+        config: {
+          circles: [{ position: "bottom left", size: "600px", opacity: "2%" }],
+        },
+      },
+      contentBackgroundColor: "false",
+    },
+  },
+  {
+    template_key: 11,
+    data: {
+      heading: "Build a Memorable Personal Brand, Without Hiring a Team",
+      description:
+        "Your all-in-one personal brand OS — from strategy to content, made simple.",
+      imageUrl:
+        "https://s3-alpha-sig.figma.com/img/707e/c151/75eeca732fe81415fc6105b91f38697f?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=SrN5DdIFFqvCpiAvAqXW85YS0W6yCIPBySsFyUAZFftWH~BRfVTfq6Wn3HE5sHPc6I6P4Ctrq4-M87sKBKVaqZvl2xTirExbOtormQ4QMNcVxwXyHWr2eH7JDeCe3Rz0BPZkrXbxQqkIUy86mJJ5h7YYr-zeTGE6ao7BtcoC8P1qGjg4XbXR8YsifZIMn4pnYpaYoSi1wNOBolik4z~VMc6My2OKYqlJSq28KhqL4NVTXs79Wc5Shu6LCQ09BzU02U3iGP8nMvvOisqhsjeUnkwbR~8vDliij6CkAB1TKLLDFRWNBvrBASMbgH38cYtMRu-7aHyQJLgW1d20yHpr4Q__", // Replace with your actual image
+      imageAlt: "Professional headshot",
+      buttonText: "Get Started",
+      communityButtonText: "JOIN OUR COMMUNITY",
+      ctaButtonText: "Start Free Trial",
+    },
+    commonConfig: {
+      width: 1584, // Use width from useCanvasDimensions hook
+      height: 396, // Use height from useCanvasDimensions hook
+      fontSize: {
+        heading: "48px",
+        description: "32px",
+        button: 20,
+      },
+      textColor: "white",
+      backgroundBackdropConfig: {
+        backgroundBackdropUrl: "",
+        backgroundBackdropPosition: "",
+        backgroundBackdropSize: "",
+      },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "",
+        headshotBackdropPosition: "",
+        headshotBackdropSize: "",
+      },
       backgroundStyle: {
         type: "radial",
         config: {
@@ -380,7 +448,7 @@ export const styles: StyleItem[] = [
     },
   },
   {
-    template_key: 11,
+    template_key: 12,
     data: {
       heading: "Build a Memorable Personal Brand, Without Hiring a Team",
       description:
@@ -405,6 +473,11 @@ export const styles: StyleItem[] = [
         backgroundBackdropPosition: "",
         backgroundBackdropSize: "",
       },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "",
+        headshotBackdropPosition: "",
+        headshotBackdropSize: "",
+      },
       backgroundStyle: {
         type: "solid",
       },
@@ -412,7 +485,7 @@ export const styles: StyleItem[] = [
     },
   },
   {
-    template_key: 11,
+    template_key: 13,
     data: {
       heading: "Build a Memorable Personal Brand, Without Hiring a Team",
       description:
@@ -437,6 +510,11 @@ export const styles: StyleItem[] = [
           "https://i.postimg.cc/X77p3rVS/Group-1000006773.png",
         backgroundBackdropPosition: "top left",
         backgroundBackdropSize: "contain",
+      },
+      headshotBackdropConfig: {
+        headshotBackdropUrl: "",
+        headshotBackdropPosition: "",
+        headshotBackdropSize: "",
       },
       backgroundStyle: {
         type: "solid",
